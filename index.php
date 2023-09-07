7e033888edeb70373c479e7529bb7bdf3cefadfc
Elearning <abdulalim.elearning@gmail.com>
2023-09-07 14:54:00 +0600

    Database Connectivity (using MySQLi)

--- index.php
+++ index.php
@@ -1,6 +1,28 @@
 <?php
-// Sessions:
-session_start();
-$_SESSION['username'] = 'john_doe';
+// Database Connectivity (using MySQLi):
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "mydb";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT * FROM users";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        echo "Name: " . $row["name"] . "<br>";
+    }
+} else {
+    echo "No results found.";
+}
+
+$conn->close();
+
 
 ?>
