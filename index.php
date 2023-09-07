0e2f941fe80b6f96039aa3d056297f47350df59f
Elearning <abdulalim.elearning@gmail.com>
2023-09-07 14:52:40 +0600

    Super Globals

--- index.php
+++ index.php
@@ -1,40 +1,4 @@
 <?php
-// Polymorphism (Interface):
-interface Shape {
-    public function calculateArea();
-}
-
-class Circle implements Shape {
-    private $radius;
-
-    function __construct($radius) {
-        $this->radius = $radius;
-    }
-
-    public function calculateArea() {
-        return 3.14 * $this->radius * $this->radius;
-    }
-}
-
-class Rectangle implements Shape {
-    private $width;
-    private $height;
-
-    function __construct($width, $height) {
-        $this->width = $width;
-        $this->height = $height;
-    }
-
-    public function calculateArea() {
-        return $this->width * $this->height;
-    }
-}
-
-$circle = new Circle(5);
-$rectangle = new Rectangle(4, 6);
-
-echo "Circle Area: " . $circle->calculateArea() . "<br>";
-echo "Rectangle Area: " . $rectangle->calculateArea();
-
-
+// Super Globals:
+echo "Your IP address is: " . $_SERVER['REMOTE_ADDR'];
 ?>
